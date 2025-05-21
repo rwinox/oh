09db801f310189414ce90141f2c22b970e74177a
Rwin <62699574+rwinox@users.noreply.github.com>
2025-05-22 02:28:09 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,23 +3,11 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
     "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
-    "https://t.me/s/v2rayfree",
-    "https://t.me/s/FreeV2rays",
-    "https://t.me/s/v2ray_free_conf",
-    "https://t.me/s/PrivateVPNs",
-    "https://t.me/s/IP_CF_Config",
-    "https://t.me/s/prrofile_purple",
-    "https://t.me/s/meli_proxyy",
-    "https://t.me/s/DirectVPN",
-    "https://t.me/s/VmessProtocol",
-    "https://t.me/s/vpnfail_vless",
-    "https://t.me/s/DailyV2RY",
-    "https://t.me/s/shadowproxy66",
-    "https://t.me/s/moftconfig",
-    "https://t.me/s/ConfigsHUB2",
-    "https://t.me/s/ArV2ray",
+    "https://t.me/s/ZibaNabz",
+    "https://t.me/s/nufilter",
+    "https://t.me/s/Outline_Vpn",
+    "https://t.me/s/zedmodeonVPN",
     # Add more URLs here if you want to include additional sources.
 ]
 
